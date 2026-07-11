ac68cba67bbc39dce0e619fef5521efb1204f082
Oussama KERSIT <kersiit@gmail.com>
2026-07-11 12:07:22 +0100

    Refactor server request handling to improve routing and response structure

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 //? ///////////////////////////////
 //? Files
@@ -39,8 +40,20 @@ const http = require("http");
 //? Server
 
 const server = http.createServer((req, res) => {
-  console.log(res);
-  res.end("Hello from the server!!");
+  const urlPathName = req.url;
+
+  if (urlPathName === "/" || urlPathName === "overview") {
+    res.end("This is the overview");
+  } else if (urlPathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-custom-http-header":
+        "Welcome to the backend of KR API Powered by NodeJS",
+    });
+    res.end("<h1>Page not found!!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
