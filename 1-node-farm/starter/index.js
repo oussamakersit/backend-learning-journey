24895ac7bf66fac755b6018e92fac2282bf57293
Oussama KERSIT <kersiit@gmail.com>
2026-07-13 17:03:22 +0100

    Enhance server routes and template rendering for product details and overview

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -39,22 +39,69 @@ const url = require("url");
 //? ///////////////////////////////
 //? Server
 
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/temp-card.html`,
+  "utf-8",
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/temp-overview.html`,
+  "utf-8",
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/temp-product.html`,
+  "utf-8",
+);
+
+const placeholderChange = (temp, product) => {
+  let output = temp.replace(/{#PRODUCTNAME#}/g, product.productName);
+  output = output.replace(/{#IMAGE#}/g, product.image);
+  output = output.replace(/{#PRICE#}/g, product.price);
+  output = output.replace(/{#FROM#}/g, product.from);
+  output = output.replace(/{#NUTRIENTS#}/g, product.nutrients);
+  output = output.replace(/{#QUANTITY#}/g, product.quantity);
+  output = output.replace(/{#DESCRIPTION#}/g, product.description);
+  output = output.replace(/{#ID#}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{#NOT_ORGANIC#}/g, "not-organic");
+
+  return output;
+};
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = data;
+
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const urlPathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // ======= ROOT Route =======
-  if (urlPathName === "/" || urlPathName === "overview") {
-    res.end("This is the overview");
+  if (pathname === "/" || pathname === "overview") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    const htmlCards = dataObj
+      .map((el) => placeholderChange(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{#PRODUCT_CARD#}", htmlCards);
+
+    // console.log(output);
+
+    res.end(output);
 
     // ======= Product Route =======
-  } else if (urlPathName === "/product") {
-    res.end("This is the product");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "content-type": "text-html" });
+
+    const product = dataObj[query.id];
+    const output = placeholderChange(tempProduct, product);
+
+    res.end(output);
 
     // ======= API Route =======
-  } else if (urlPathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
