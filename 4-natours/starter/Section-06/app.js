8c987d53bfd5a9fe4a4b5e4ccb3de7e8cd36f9d7
Oussama KERSIT <kersiit@gmail.com>
2026-08-10 12:00:47 +0100

    refactor: reorganize middleware setup in app.js for clarity

--- 4-natours/starter/Section-06/app.js
+++ 4-natours/starter/Section-06/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 
 const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
+
+const app = express();
+
+// Built-in Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log(`Hello from the Middleware 👋`);
@@ -14,10 +20,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// Built-in Middleware
-app.use(morgan('dev'));
-app.use(express.json());
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
