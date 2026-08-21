867d3c74580453830524b525085c57e320199a2e
Oussama KERSIT <kersiit@gmail.com>
2026-08-21 23:25:23 +0100

    Adding connection into remote database + fixing DNS problem

--- 4-natours/starter/Section-06/app.js
+++ 4-natours/starter/Section-06/app.js
@@ -7,7 +7,10 @@ const userRouter = require('./Routes/userRoutes');
 const app = express();
 
 // Built-in Middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- 4-natours/starter/Section-06/server.js
+++ 4-natours/starter/Section-06/server.js
@@ -1,6 +1,26 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const dns = require('dns');
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const PORT = 8000;
-app.listen(PORT, '127.0.0.1', () =>
-  console.log(`Server is listening on port: ${PORT}...`),
+const DB = process.env.REMOTE_DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
 );
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log(`DB connected successfully`);
+  })
+  .catch((err) => {
+    console.log(`DB connection failed: ${err}`);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}...`));
+
+// console.log(process.env);
