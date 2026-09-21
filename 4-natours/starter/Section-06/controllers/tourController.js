fb332386deda6d22025a85f4263cd8c4f9cfd563
Oussama KERSIT <kersiit@gmail.com>
2026-09-21 15:36:12 +0000

    Untrack sensitive environment configuration file

--- 4-natours/starter/Section-06/controllers/tourController.js
+++ 4-natours/starter/Section-06/controllers/tourController.js
@@ -45,6 +45,19 @@ exports.getAllTours = async (req, res) => {
       // Here we exclude the __v from the default value (In the context of APIs, especially when working with MongoDB and Mongoose (the Node.js ODM for MongoDB), __v is a field automatically added to documents)
     }
 
+    // 4) Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10, page 1, 11-20 page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //? Execute the Query
     const tours = await query;
 
